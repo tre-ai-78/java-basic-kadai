484370fb83a5ae9de041d64f843c9f12fa38200b
tre-ai-78 <trezor78@gmail.com>
2026-03-04 16:42:26 +0900

    修正

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -3,7 +3,7 @@ package kadai_015;
 public class CarExec_Chapter15 {
 
 	public static void main(String[] args) {
-		Car_Chapter15 car = new Car_Chapter15(1, 10);
+		Car_Chapter15 car = new Car_Chapter15();
 		
 		car.changeGear(3);
 		
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -2,17 +2,15 @@ package kadai_015;
 
 public class Car_Chapter15 {
 	
-	private int gear = 0;
-	private int speed = 0;
-	
-	public Car_Chapter15( int gear, int speed) {
-		this.gear = gear;
-		this.speed = speed;
-	}
+	private int gear = 1;
+	private int speed = 10;
 	
 	public void changeGear(int afterGear) {
 		
-		this.speed = switch(afterGear) {
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+		this.gear = afterGear;
+		
+		this.speed = switch(this.gear) {
 		case 1 -> 10;
 		case 2 -> 20;
 		case 3 -> 30;
@@ -21,11 +19,10 @@ public class Car_Chapter15 {
 		default -> 10;
 		};
 		
-		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
 	}
 	
 	public void run() {
-		System.out.println("速度は時速" + this.speed + "です");
+		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 
 }
